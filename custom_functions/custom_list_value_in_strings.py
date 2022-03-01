42519ccb542657d9cf827bd994c7cc9f086e7ac3
admin <root@localhost>
2022-03-01 12:27:28 +0000

    dd

--- custom_functions/custom_list_value_in_strings.py
+++ custom_functions/custom_list_value_in_strings.py
@@ -24,7 +24,7 @@ def custom_list_value_in_strings(custom_list=None, comparison_strings=None, **kw
     misses = []
 
     # Loop through each comparison string
-    phantom.debug(comparison_strings)
+    phantom.debug('comparison_strings: {}'.format(comparison_strings))
     for comparison_string in comparison_strings:
 
         # Loop through the custom list to see if any list value is found in the comparison string
