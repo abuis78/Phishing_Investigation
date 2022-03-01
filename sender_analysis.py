ce6df1e78af5ff94729d074aad8b98e187a0e60b
admin <root@localhost>
2022-03-01 13:11:26 +0000

    ss

--- sender_analysis.py
+++ sender_analysis.py
@@ -199,7 +199,7 @@ def extract_domain_from_dkim_signature(action=None, success=None, container=None
 
     # Write your custom code here...
     import re
-    
+    phantom.debug(container_artifact_cef_item_0)
     domain_regex = r'((?!-))(xn--)?[a-z0-9][a-z0-9-_]{0,61}[a-z0-9]{0,1}\.(xn--)?([a-z0-9\-]{1,61}|[a-z0-9-]{1,30}\.[a-z]{2,})'
     dkim_domain = re.findall(domain_regex, container_artifact_cef_item_0, re.IGNORECASE)
     phantom.debug(dkim_domain)
