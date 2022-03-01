72f59818b43480d67bc6d3ef0e29274cde3ea439
admin <root@localhost>
2022-03-01 12:29:03 +0000

    ss

--- sender_analysis.py
+++ sender_analysis.py
@@ -39,7 +39,7 @@ def extract_email_from_emailheaders(action=None, success=None, container=None, r
     ## Custom Code End
     ################################################################################
 
-    phantom.custom_function(custom_function="Phishing_Investigation/regex_extract_email", parameters=parameters, name="extract_email_from_emailheaders", callback=custom_list_value_in_strings_3)
+    phantom.custom_function(custom_function="Phishing_Investigation/regex_extract_email", parameters=parameters, name="extract_email_from_emailheaders", callback=custom_list_value_in_strings_4)
 
     return
 
@@ -200,19 +200,19 @@ def debug_2(action=None, success=None, container=None, results=None, handle=None
     return
 
 
-def custom_list_value_in_strings_3(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug("custom_list_value_in_strings_3() called")
+def custom_list_value_in_strings_4(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("custom_list_value_in_strings_4() called")
 
     extract_email_from_emailheaders_data = phantom.collect2(container=container, datapath=["extract_email_from_emailheaders:custom_function_result.data.*.email_address"])
 
-    extract_email_from_emailheaders_data___email_address = [item[0] for item in extract_email_from_emailheaders_data]
-
     parameters = []
 
-    parameters.append({
-        "custom_list": "VIP",
-        "comparison_strings": extract_email_from_emailheaders_data___email_address,
-    })
+    # build parameters list for 'custom_list_value_in_strings_4' call
+    for extract_email_from_emailheaders_data_item in extract_email_from_emailheaders_data:
+        parameters.append({
+            "custom_list": "VIP",
+            "comparison_strings": extract_email_from_emailheaders_data_item[0],
+        })
 
     ################################################################################
     ## Custom Code Start
@@ -224,7 +224,7 @@ def custom_list_value_in_strings_3(action=None, success=None, container=None, re
     ## Custom Code End
     ################################################################################
 
-    phantom.custom_function(custom_function="Phishing_Investigation/custom_list_value_in_strings", parameters=parameters, name="custom_list_value_in_strings_3")
+    phantom.custom_function(custom_function="Phishing_Investigation/custom_list_value_in_strings", parameters=parameters, name="custom_list_value_in_strings_4")
 
     return
 
