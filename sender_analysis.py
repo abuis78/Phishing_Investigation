c54c781a9c0eda8b429b8d64eac5bd4c77684014
admin <root@localhost>
2022-03-01 14:59:27 +0000

    ss

--- sender_analysis.py
+++ sender_analysis.py
@@ -401,12 +401,15 @@ def search_vor_company_keywords_in_email(action=None, success=None, container=No
     
     for item in keywoard_list:
         ergebnis = re.findall(item, extract_email_from_emailheaders_data___email_address[0], re.IGNORECASE)
-        phantom.debug(len(ergebnis))
+        #phantom.debug(len(ergebnis))
         for x in ergebnis:
             if ergebnis != -1:
                 matches.append({"match": x})
             else:
                 misses.append({"miss": x})
+                
+    phantom.debug(matches)
+    phantom.debug(misses)    
     
     match_count = len(matches)
     miss_count = len(misses)
