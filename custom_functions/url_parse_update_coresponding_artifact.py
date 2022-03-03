54cf065f0a8add5afd34d35f0bcb7ebdfd48ae15
admin <root@localhost>
2022-03-03 10:16:17 +0000

    tg

--- custom_functions/url_parse_update_coresponding_artifact.py
+++ custom_functions/url_parse_update_coresponding_artifact.py
@@ -21,15 +21,68 @@ def url_parse_update_coresponding_artifact(input_url=None, artifact_id=None, **k
     from urllib.parse import urlparse
     
     outputs = {}
+    json_dict = None
+    valid_keys = [
+        'artifact_type', 'cef', 'cef_data', 'cef_types', 'container', 'container_id',  
+        'field_mapping', 'data', 'description', 'end_time', 'has_note', 'identifier', 
+        'ingest_app', 'ingest_app_id', 'kill_chain', 'label', 'name', 'owner_id', 
+        'parent_container', 'parent_artifact', 'raw_data', 'run_automation', 'severity',
+        'source_data_identifier', 'start_time', 'tags', 'type'
+    ]    
     if input_url:
         parsed = urlparse(input_url)
         outputs = {'scheme': parsed.scheme, 'netloc': parsed.netloc, 'path': parsed.path, 'params': parsed.params, 'query': parsed.query, 'fragment': parsed.fragment, 'output_url': input_url}
         
+        input_json = { 'cef':{'scheme': parsed.scheme, 'netloc': parsed.netloc, 'path': parsed.path, 'params': parsed.params, 'query': parsed.query, 'fragment': parsed.fragment, 'output_url': input_url}}
         #updating the coresponding artifact
         if artifact_id:
             art_url = phantom.build_phantom_rest_url('artifact', artifact_id)
-            data = { 'cef':{'scheme': parsed.scheme, 'netloc': parsed.netloc, 'path': parsed.path, 'params': parsed.params, 'query': parsed.query, 'fragment': parsed.fragment, 'output_url': input_url}}
-            response_data = phantom.requests.post(art_url, json=data, verify=False).json()
+            # get data from artifakt
+            updated_artifact = phantom.requests.get(art_url, verify=False).json()
+                
+            if input_json:
+                # ensure valid input_json
+                if isinstance(input_json, dict):
+                    json_dict = input_json
+                elif isinstance(input_json, str):
+                    json_dict = json.loads(input_json)
+                else:
+                    raise ValueError("input_json must be either 'dict' or valid json 'string'")   
+                    
+            if json_dict:
+                # Merge dictionaries, using the value from json_dict if there are any conflicting keys
+                for json_key in json_dict:
+                    if json_key in valid_keys:
+                        # translate keys supported in phantom.add_artifact() to their corresponding values in /rest/artifact
+                        if json_key == 'container':
+                            updated_artifact['container_id'] = json_dict[json_key]
+                        elif json_key == 'raw_data':
+                            updated_artifact['data'].update(json_dict[json_key])
+                        elif json_key == 'cef_data':
+                            updated_artifact['cef'].update(json_dict[json_key])
+                        elif json_key == 'identifier':
+                            updated_artifact['source_data_identifier'] = json_dict[json_key]
+                        elif json_key == 'ingest_app':
+                            updated_artifact['ingest_app_id'] = json_dict[json_key]
+                        elif json_key == 'artifact_type':
+                            updated_artifact['type'] = json_dict[json_key]
+                        elif json_key == 'field_mapping':
+                            updated_artifact['cef_types'].update(json_dict[json_key])
+                        else:
+                            if isinstance(updated_artifact[json_key], dict):
+                                updated_artifact[json_key].update(json_dict[json_key])
+                            elif isinstance(updated_artifact[json_key], list):
+                                updated_artifact[json_key].append(json_dict[json_key])
+                                updated_artifact[json_key] = list(set(updated_artifact[json_key]))
+                            else:
+                                updated_artifact[json_key] = json_dict[json_key]
+                    else:
+                        phantom.debug(f"Unsupported key: '{json_key}'")
+                        
+            
+            phantom.debug('Updating artifact {} with the following attributes:\n{}'.format(artifact_id, updated_artifact))
+
+            response_data = phantom.requests.post(art_url, json=updated_artifact, verify=False).json()
             phantom.debug(response_data)
     # Return a JSON-serializable object
     assert json.dumps(outputs)  # Will raise an exception if the :outputs: object is not JSON-serializable
