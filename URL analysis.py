6a45e4e03d50ccc83b74051833555b6fcf72c40b
admin <root@localhost>
2022-03-03 07:46:14 +0000

    tr

--- URL analysis.py
+++ URL analysis.py
@@ -11,13 +11,13 @@ from datetime import datetime, timedelta
 def on_start(container):
     phantom.debug('on_start() called')
 
-    # call 'filter_1' block
-    filter_1(container=container)
+    # call 'filter_url_artifact' block
+    filter_url_artifact(container=container)
 
     return
 
-def filter_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug("filter_1() called")
+def filter_url_artifact(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("filter_url_artifact() called")
 
     # collect filtered artifact ids and results for 'if' condition 1
     matched_artifacts_1, matched_results_1 = phantom.condition(
@@ -25,7 +25,7 @@ def filter_1(action=None, success=None, container=None, results=None, handle=Non
         conditions=[
             ["artifact:*.name", "==", "URL Artifact"]
         ],
-        name="filter_1:condition_1")
+        name="filter_url_artifact:condition_1")
 
     # call connected blocks if filtered artifacts or results
     if matched_artifacts_1 or matched_results_1:
