5e535edb186ab8098ebf47c1b71d950dfce55c0f
admin <root@localhost>
2022-03-03 07:54:17 +0000

    tr

--- URL analysis.py
+++ URL analysis.py
@@ -29,7 +29,81 @@ def filter_url_artifact(action=None, success=None, container=None, results=None,
 
     # call connected blocks if filtered artifacts or results
     if matched_artifacts_1 or matched_results_1:
-        pass
+        separate_a_url_into_components(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+
+    return
+
+
+def separate_a_url_into_components(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("separate_a_url_into_components() called")
+
+    filtered_artifact_0_data_filter_url_artifact = phantom.collect2(container=container, datapath=["filtered-data:filter_url_artifact:condition_1:artifact:*.cef.requestURL","filtered-data:filter_url_artifact:condition_1:artifact:*.id"])
+
+    parameters = []
+
+    # build parameters list for 'separate_a_url_into_components' call
+    for filtered_artifact_0_item_filter_url_artifact in filtered_artifact_0_data_filter_url_artifact:
+        parameters.append({
+            "input_url": filtered_artifact_0_item_filter_url_artifact[0],
+        })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.custom_function(custom_function="Phishing_Investigation/url_parse", parameters=parameters, name="separate_a_url_into_components", callback=update_artifact_1)
+
+    return
+
+
+def update_artifact_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("update_artifact_1() called")
+
+    # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+
+    cef_json_formatted_string = phantom.format(
+        container=container,
+        template="""{{ \"scheme\": \"{0}\",\"netloc\": \"{1}\",\"path\": \"{2}\",\"params\": \"{3}\",\"query\": \"{4}\",\"fragment\": \"{5}\",\"output_url\": \"{6}\"  \n\n}}\n""",
+        parameters=[
+            "separate_a_url_into_components:custom_function_result.data.scheme",
+            "separate_a_url_into_components:custom_function_result.data.netloc",
+            "separate_a_url_into_components:custom_function_result.data.path",
+            "separate_a_url_into_components:custom_function_result.data.params",
+            "separate_a_url_into_components:custom_function_result.data.query",
+            "separate_a_url_into_components:custom_function_result.data.fragment",
+            "separate_a_url_into_components:custom_function_result.data.output_url"
+        ])
+
+    filtered_artifact_0_data_filter_url_artifact = phantom.collect2(container=container, datapath=["filtered-data:filter_url_artifact:condition_1:artifact:*.id","filtered-data:filter_url_artifact:condition_1:artifact:*.id"])
+
+    parameters = []
+
+    # build parameters list for 'update_artifact_1' call
+    for filtered_artifact_0_item_filter_url_artifact in filtered_artifact_0_data_filter_url_artifact:
+        if filtered_artifact_0_item_filter_url_artifact[0] is not None:
+            parameters.append({
+                "artifact_id": filtered_artifact_0_item_filter_url_artifact[0],
+                "cef_json": cef_json_formatted_string,
+                "context": {'artifact_id': filtered_artifact_0_item_filter_url_artifact[1]},
+            })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.act("update artifact", parameters=parameters, name="update_artifact_1", assets=["phantom"])
 
     return
 
