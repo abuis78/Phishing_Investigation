3c7951cc150b3ed5335044b4d6b2fe1c7f95a17a
admin <root@localhost>
2022-03-03 08:07:17 +0000

    tg

--- custom_functions/url_parse.py
+++ custom_functions/url_parse.py
@@ -1,9 +1,10 @@
-def url_parse(input_url=None, **kwargs):
+def url_parse(input_url=None, artifact_id=None, **kwargs):
     """
     Separate a URL into its components using urlparse() from the urllib module of Python 3.
     
     Args:
         input_url (CEF type: url): The URL to parse
+        artifact_id
     
     Returns a JSON-serializable object that implements the configured data paths:
         scheme: The scheme of the URL, such as HTTP, HTTPS, or FTP.
@@ -13,6 +14,7 @@ def url_parse(input_url=None, **kwargs):
         query: The query string of the URL after the question mark. Multiple parameters are not separated from each other.
         fragment: The subcomponent of the resource which is identified after the hash sign.
         output_url (CEF type: url): Passthrough of the original url
+        context_id
     """
     ############################ Custom Code Goes Below This Line #################################
     import json
@@ -22,7 +24,7 @@ def url_parse(input_url=None, **kwargs):
     outputs = {}
     if input_url:
         parsed = urlparse(input_url)
-        outputs = {'scheme': parsed.scheme, 'netloc': parsed.netloc, 'path': parsed.path, 'params': parsed.params, 'query': parsed.query, 'fragment': parsed.fragment, 'output_url': input_url}
+        outputs = {'scheme': parsed.scheme, 'netloc': parsed.netloc, 'path': parsed.path, 'params': parsed.params, 'query': parsed.query, 'fragment': parsed.fragment, 'output_url': input_url, 'context_id': artifact_id}
                 
     # Return a JSON-serializable object
     assert json.dumps(outputs)  # Will raise an exception if the :outputs: object is not JSON-serializable
