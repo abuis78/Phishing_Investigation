75b2ce1f3a98ebeb12ee4735c373199dfaf9baae
admin <root@localhost>
2022-03-10 06:28:20 +0000

    df

--- file analysis.py
+++ file analysis.py
@@ -11,9 +11,222 @@ from datetime import datetime, timedelta
 def on_start(container):
     phantom.debug('on_start() called')
 
+    # call 'decision_1' block
+    decision_1(container=container)
+
+    return
+
+def decision_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("decision_1() called")
+
+    # check for 'if' condition 1
+    found_match_1 = phantom.decision(
+        container=container,
+        conditions=[
+            ["Vault Artifact", "in", "artifact:*.name"]
+        ])
+
+    # call connected blocks if condition 1 matched
+    if found_match_1:
+        filter_1(action=action, success=success, container=container, results=results, handle=handle)
+        return
+
+    # check for 'else' condition 2
+    workbook_task_update_1(action=action, success=success, container=container, results=results, handle=handle)
+
+    return
+
+
+def filter_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("filter_1() called")
+
+    # collect filtered artifact ids and results for 'if' condition 1
+    matched_artifacts_1, matched_results_1 = phantom.condition(
+        container=container,
+        conditions=[
+            ["artifact:*.name", "==", "Vault Artifact"]
+        ],
+        name="filter_1:condition_1")
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_1 or matched_results_1:
+        file_reputation_1(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+
+    return
+
+
+def file_reputation_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("file_reputation_1() called")
+
+    # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+
+    container_artifact_data = phantom.collect2(container=container, datapath=["artifact:*.cef.fileHash","artifact:*.id"])
+
+    parameters = []
+
+    # build parameters list for 'file_reputation_1' call
+    for container_artifact_item in container_artifact_data:
+        if container_artifact_item[0] is not None:
+            parameters.append({
+                "hash": container_artifact_item[0],
+                "context": {'artifact_id': container_artifact_item[1]},
+            })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.act("file reputation", parameters=parameters, name="file_reputation_1", assets=["virustotal v3"], callback=filter_status)
+
+    return
+
+
+def workbook_task_update_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("workbook_task_update_1() called")
+
+    id_value = container.get("id", None)
+
+    parameters = []
+
+    parameters.append({
+        "task_name": "email artefacts",
+        "note_title": "[Automated completion] file analysis",
+        "note_content": "file analysis",
+        "status": "complete",
+        "owner": "current",
+        "container": id_value,
+    })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.custom_function(custom_function="Phishing_Investigation/workbook_task_update", parameters=parameters, name="workbook_task_update_1")
+
+    return
+
+
+def filter_status(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("filter_status() called")
+
+    # collect filtered artifact ids and results for 'if' condition 1
+    matched_artifacts_1, matched_results_1 = phantom.condition(
+        container=container,
+        conditions=[
+            ["file_reputation_1:action_result.status", "==", "failed"]
+        ],
+        name="filter_status:condition_1")
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_1 or matched_results_1:
+        update_artifact_1(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+
+    # collect filtered artifact ids and results for 'if' condition 2
+    matched_artifacts_2, matched_results_2 = phantom.condition(
+        container=container,
+        conditions=[
+            ["file_reputation_1:action_result.status", "==", "success"]
+        ],
+        name="filter_status:condition_2")
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_2 or matched_results_2:
+        update_artifact_2(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_2, filtered_results=matched_results_2)
+
+    return
+
+
+def update_artifact_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("update_artifact_1() called")
+
+    # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+
+    filtered_result_0_data_filter_status = phantom.collect2(container=container, datapath=["filtered-data:filter_status:condition_1:file_reputation_1:action_result.parameter.context.artifact_id"])
+
+    parameters = []
+
+    # build parameters list for 'update_artifact_1' call
+    for filtered_result_0_item_filter_status in filtered_result_0_data_filter_status:
+        if filtered_result_0_item_filter_status[0] is not None:
+            parameters.append({
+                "artifact_id": filtered_result_0_item_filter_status[0],
+                "tags": "status_failed",
+            })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.act("update artifact", parameters=parameters, name="update_artifact_1", assets=["phantom"])
+
+    return
+
+
+def update_artifact_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("update_artifact_2() called")
+
+    # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+
+    cef_json_formatted_string = phantom.format(
+        container=container,
+        template="""{{ \"VT_message\": \"{0}\" }}""",
+        parameters=[
+            "filtered-data:filter_status:condition_2:file_reputation_1:action_result.message"
+        ])
+
+    filtered_result_0_data_filter_status = phantom.collect2(container=container, datapath=["filtered-data:filter_status:condition_2:file_reputation_1:action_result.parameter.context.artifact_id"])
+
+    parameters = []
+
+    # build parameters list for 'update_artifact_2' call
+    for filtered_result_0_item_filter_status in filtered_result_0_data_filter_status:
+        if filtered_result_0_item_filter_status[0] is not None:
+            parameters.append({
+                "artifact_id": filtered_result_0_item_filter_status[0],
+                "tags": "status_success",
+                "cef_json": cef_json_formatted_string,
+            })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.act("update artifact", parameters=parameters, name="update_artifact_2", assets=["phantom"], callback=severity_set_based_on_malicious)
+
+    return
+
+
+def severity_set_based_on_malicious(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("severity_set_based_on_malicious() called")
+
 
 
     return
+
 
 def on_finish(container, summary):
     phantom.debug("on_finish() called")
