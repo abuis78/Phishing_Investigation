a9145d5c22f054b39293532b6e76405feafdd668
admin <root@localhost>
2022-03-10 07:49:50 +0000

    df

--- file analysis.py
+++ file analysis.py
@@ -32,7 +32,7 @@ def decision_1(action=None, success=None, container=None, results=None, handle=N
         return
 
     # check for 'else' condition 2
-    workbook_task_update_1(action=action, success=success, container=container, results=results, handle=handle)
+    join_workbook_task_update_1(action=action, success=success, container=container, results=results, handle=handle)
 
     return
 
@@ -83,6 +83,16 @@ def file_reputation_1(action=None, success=None, container=None, results=None, h
     ################################################################################
 
     phantom.act("file reputation", parameters=parameters, name="file_reputation_1", assets=["virustotal v3"], callback=filter_status)
+
+    return
+
+
+def join_workbook_task_update_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("join_workbook_task_update_1() called")
+
+    if phantom.completed(custom_function_names=["noop_2"]):
+        # call connected block "workbook_task_update_1"
+        workbook_task_update_1(container=container, handle=handle)
 
     return
 
@@ -175,7 +185,7 @@ def update_artifact_1(action=None, success=None, container=None, results=None, h
     ## Custom Code End
     ################################################################################
 
-    phantom.act("update artifact", parameters=parameters, name="update_artifact_1", assets=["phantom"])
+    phantom.act("update artifact", parameters=parameters, name="update_artifact_1", assets=["phantom"], callback=join_noop_2)
 
     return
 
@@ -223,7 +233,129 @@ def update_artifact_2(action=None, success=None, container=None, results=None, h
 def severity_set_based_on_malicious(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug("severity_set_based_on_malicious() called")
 
+    # collect filtered artifact ids and results for 'if' condition 1
+    matched_artifacts_1, matched_results_1 = phantom.condition(
+        container=container,
+        conditions=[
+            ["filtered-data:filter_status:condition_2:file_reputation_1:action_result.summary.malicious", ">", 0]
+        ],
+        name="severity_set_based_on_malicious:condition_1")
 
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_1 or matched_results_1:
+        update_artifact_3(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+
+    # collect filtered artifact ids and results for 'if' condition 2
+    matched_artifacts_2, matched_results_2 = phantom.condition(
+        container=container,
+        conditions=[
+            ["filtered-data:filter_status:condition_2:file_reputation_1:action_result.summary.malicious", "==", 0]
+        ],
+        name="severity_set_based_on_malicious:condition_2")
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_2 or matched_results_2:
+        update_artifact_4(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_2, filtered_results=matched_results_2)
+
+    return
+
+
+def update_artifact_3(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("update_artifact_3() called")
+
+    # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+
+    filtered_result_0_data_severity_set_based_on_malicious = phantom.collect2(container=container, datapath=["filtered-data:severity_set_based_on_malicious:condition_1:file_reputation_1:action_result.parameter.context.artifact_id"])
+
+    parameters = []
+
+    # build parameters list for 'update_artifact_3' call
+    for filtered_result_0_item_severity_set_based_on_malicious in filtered_result_0_data_severity_set_based_on_malicious:
+        if filtered_result_0_item_severity_set_based_on_malicious[0] is not None:
+            parameters.append({
+                "artifact_id": filtered_result_0_item_severity_set_based_on_malicious[0],
+                "severity": "high",
+            })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.act("update artifact", parameters=parameters, name="update_artifact_3", assets=["phantom"], callback=join_noop_2)
+
+    return
+
+
+def update_artifact_4(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("update_artifact_4() called")
+
+    # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+
+    filtered_result_0_data_severity_set_based_on_malicious = phantom.collect2(container=container, datapath=["filtered-data:severity_set_based_on_malicious:condition_2:file_reputation_1:action_result.parameter.context.artifact_id"])
+
+    parameters = []
+
+    # build parameters list for 'update_artifact_4' call
+    for filtered_result_0_item_severity_set_based_on_malicious in filtered_result_0_data_severity_set_based_on_malicious:
+        if filtered_result_0_item_severity_set_based_on_malicious[0] is not None:
+            parameters.append({
+                "artifact_id": filtered_result_0_item_severity_set_based_on_malicious[0],
+                "severity": "low",
+            })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.act("update artifact", parameters=parameters, name="update_artifact_4", assets=["phantom"], callback=join_noop_2)
+
+    return
+
+
+def join_noop_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("join_noop_2() called")
+
+    # if the joined function has already been called, do nothing
+    if phantom.get_run_data(key="join_noop_2_called"):
+        return
+
+    # save the state that the joined function has now been called
+    phantom.save_run_data(key="join_noop_2_called", value="noop_2")
+
+    # call connected block "noop_2"
+    noop_2(container=container, handle=handle)
+
+    return
+
+
+def noop_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("noop_2() called")
+
+    parameters = [{}]
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.custom_function(custom_function="Phishing_Investigation/noop", parameters=parameters, name="noop_2", callback=join_workbook_task_update_1)
 
     return
 
