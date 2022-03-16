ac25e79e9a614d0c508ee17c7cea84dcdfbb803c
admin <root@localhost>
2022-03-16 20:10:07 +0000

    dd

--- custom_functions/container_list_various_conditions.py
+++ custom_functions/container_list_various_conditions.py
@@ -21,7 +21,7 @@ def container_list_various_conditions(filter_condition=None, time_span=None, con
     u1 = phantom.build_phantom_rest_url('container',container_id)
     phantom.debug(u1)
     r1 = phantom.requests.get(u1,verify=False)  
-    c1 = r1.json()["data"]
+    c1 = r1.json()
     phantom.debug(c1)
     t2 = datetime.strptime(c1["create_time"], "%Y-%m-%dT%H:%M:%S.%fZ")
     phantom.debug(t2)
