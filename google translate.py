8db80c07c10ffbe012f195eca9c05d1970ead3f6
admin <root@localhost>
2022-03-26 15:08:56 +0000

    jj

--- google translate.py
+++ google translate.py
@@ -49,7 +49,7 @@ def detect_language(action=None, success=None, container=None, results=None, han
     phantom.debug("phantom returned status with message {}".format(response.text))
     
     detect_language__language = response.json()
-    phantom.debug(detect_language__language["data"]["detections"]["language"])
+    phantom.debug(detect_language__language["data"]["detections"][0])
     
     
     ################################################################################
