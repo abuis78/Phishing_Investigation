240fa181d01b1e6074ba7660e6b2433960053fc5
admin <root@localhost>
2022-03-27 06:17:25 +0000

    cc

--- google translate.py
+++ google translate.py
@@ -33,8 +33,8 @@ def detect_language(action=None, success=None, container=None, results=None, han
     import urllib
     phantom.debug(playbook_input_text_string_values)
     phantom.debug("---- CONVERT string -----")
-    text_string = urllib.parse.quote(playbook_input_text_string_values)
-    phantom.debug(text_string)
+    #text_string = urllib.parse.quote(playbook_input_text_string_values)
+    #phantom.debug(text_string)
     
     
     phantom.debug("---- DETECT Language-----")
@@ -46,7 +46,7 @@ def detect_language(action=None, success=None, container=None, results=None, han
         'X-RapidAPI-Key': '44aaaeef79msh75169124fb09b39p153dedjsnab8c1122a6a5'
       }
     
-    payload1 = "q=" + text_string+ ""
+    payload1 = "q=" + playbook_input_text_string_values+ ""
     
     response1 = phantom.requests.post(
         u1,
@@ -65,7 +65,7 @@ def detect_language(action=None, success=None, container=None, results=None, han
     
     u2 = "https://google-translate1.p.rapidapi.com/language/translate/v2"
     
-    payload2 = "q='" + text_string + "'&target=de&source=" + lang
+    payload2 = "q='" + playbook_input_text_string_values + "'&target=de&source=" + lang
     
     response2 = phantom.requests.post(
         u2,
