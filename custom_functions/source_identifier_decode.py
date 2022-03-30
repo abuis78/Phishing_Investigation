ff225a008753fa26c52cabfbdb942b13d4850ed0
admin <root@localhost>
2022-03-30 14:00:39 +0000

    ddd

---
+++ custom_functions/source_identifier_decode.py
@@ -0,0 +1,30 @@
+def source_identifier_decode(source_identifier=None, **kwargs):
+    """
+    Args:
+        source_identifier
+    
+    Returns a JSON-serializable object that implements the configured data paths:
+        container_id
+        pattern_1
+        pattern_2
+    """
+    ############################ Custom Code Goes Below This Line #################################
+    import json
+    import phantom.rules as phantom
+    import base64    
+    
+    outputs = {}
+    
+    # Write your custom code here...
+    decoded = base64.b64decode(source_identifier).decode()
+    info = decoded.split("|")
+    
+    container_id = info[0]
+    outputs["container_id"] = info[0]
+    outputs["pattern_1"] = info[1]
+    outputs["pattern_1"] = info[2]
+    phantom.debug('Container-ID decoded: container_id {}'.format(container_id))    
+    
+    # Return a JSON-serializable object
+    assert json.dumps(outputs)  # Will raise an exception if the :outputs: object is not JSON-serializable
+    return outputs
