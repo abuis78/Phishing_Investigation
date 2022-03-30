b8c18f3d0086923b7e0b072f798b92c59e11bcee
admin <root@localhost>
2022-03-30 13:54:20 +0000

    ftrdtf

--- custom_functions/define_source_identifier.py
+++ custom_functions/define_source_identifier.py
@@ -20,7 +20,7 @@ def define_source_identifier(container_id=None, pattern_1=None, pattern_2=None, 
     # Write your custom code here...
     outputs["source_identifier_key"] = base64.b64encode("{}|{}|{}".format(container_id, pattern_1, pattern_2).encode()).decode()
     phantom.debug(outputs["source_identifier_key"])
-    outputs["source_identifier_prefix_key"] = "[{}: {}]".format(source_identifier_prefix, outputs["source_identifier_key"])
+    outputs["source_identifier_prefix_key"] = "{}: {}".format(source_identifier_prefix, outputs["source_identifier_key"])
     phantom.debug(outputs["source_identifier_prefix_key"])
     
     # Return a JSON-serializable object
