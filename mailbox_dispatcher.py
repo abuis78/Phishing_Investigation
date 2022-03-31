2697fbfc1e32dc8b473ab37f504930ee4f0174cc
admin <root@localhost>
2022-03-31 09:41:32 +0000

    ss

--- mailbox_dispatcher.py
+++ mailbox_dispatcher.py
@@ -85,16 +85,16 @@ def extract_source_identifier_1(action=None, success=None, container=None, resul
 def add_email_to_corresponding_contaiiner_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug("add_email_to_corresponding_contaiiner_2() called")
 
-    extract_source_identifier_1__result = phantom.collect2(container=container, datapath=["extract_source_identifier_1:custom_function_result.data.container_id"])
+    source_identifier_decode_4__result = phantom.collect2(container=container, datapath=["source_identifier_decode_4:custom_function_result.data.container_id"])
     filtered_artifact_0_data_filter_artifact_for_email_artifact = phantom.collect2(container=container, datapath=["filtered-data:filter_artifact_for_email_artifact:condition_1:artifact:*.cef.emailHeaders.decodedSubject","filtered-data:filter_artifact_for_email_artifact:condition_1:artifact:*.cef.bodyPart1","filtered-data:filter_artifact_for_email_artifact:condition_1:artifact:*.id"])
 
     parameters = []
 
     # build parameters list for 'add_email_to_corresponding_contaiiner_2' call
-    for extract_source_identifier_1__result_item in extract_source_identifier_1__result:
+    for source_identifier_decode_4__result_item in source_identifier_decode_4__result:
         for filtered_artifact_0_item_filter_artifact_for_email_artifact in filtered_artifact_0_data_filter_artifact_for_email_artifact:
             parameters.append({
-                "container_id": extract_source_identifier_1__result_item[0],
+                "container_id": source_identifier_decode_4__result_item[0],
                 "subject": filtered_artifact_0_item_filter_artifact_for_email_artifact[0],
                 "body": filtered_artifact_0_item_filter_artifact_for_email_artifact[1],
             })
