4bf3d1d1c76ffceb651f4f60bb75f8b65d5aa77f
admin <root@localhost>
2022-03-31 09:29:22 +0000

    dd

--- custom_functions/extract_source_identifier.py
+++ custom_functions/extract_source_identifier.py
@@ -6,9 +6,7 @@ def extract_source_identifier(email_subject=None, email_body=None, source_identi
         source_identifier_prefix
     
     Returns a JSON-serializable object that implements the configured data paths:
-        container_id
-        pattern_1
-        pattern_2
+        source_identifier
     """
     ############################ Custom Code Goes Below This Line #################################
     import json
@@ -24,12 +22,11 @@ def extract_source_identifier(email_subject=None, email_body=None, source_identi
     result = pattern.search(email_body)
     
     if result:
-        outputs["container_id"] = result.group(1)
-        outputs["container_id"] = True
-        phantom.debug(result.group(0))
+        outputs["source_identifier"] = result.group(1)
+        outputs["source_identifier"] = True
         phantom.debug(result.group(1))
     else:
-        outputs["container_id"] = False
+        outputs["source_identifier"] = False
 
     # Return a JSON-serializable object
     assert json.dumps(outputs)  # Will raise an exception if the :outputs: object is not JSON-serializable
