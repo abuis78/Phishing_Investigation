9d59b4a81bed6ccc365f8e3f877a183ca61939a2
admin <root@localhost>
2022-04-01 07:24:52 +0000

    dsf

--- extract case id from subject.py
+++ extract case id from subject.py
@@ -11,9 +11,111 @@ from datetime import datetime, timedelta
 def on_start(container):
     phantom.debug('on_start() called')
 
-
+    # call 'decision_1' block
+    decision_1(container=container)
 
     return
+
+def filter_email_artifact(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("filter_email_artifact() called")
+
+    # collect filtered artifact ids and results for 'if' condition 1
+    matched_artifacts_1, matched_results_1 = phantom.condition(
+        container=container,
+        conditions=[
+            ["artifact:*.name", "==", "Email Artifact"]
+        ],
+        name="filter_email_artifact:condition_1")
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_1 or matched_results_1:
+        extract_the_case_id_1(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+
+    return
+
+
+def decision_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("decision_1() called")
+
+    # check for 'if' condition 1
+    found_match_1 = phantom.decision(
+        container=container,
+        conditions=[
+            ["artifact:*.name", "==", "Email Artifact"]
+        ])
+
+    # call connected blocks if condition 1 matched
+    if found_match_1:
+        filter_email_artifact(action=action, success=success, container=container, results=results, handle=handle)
+        return
+
+    return
+
+
+def extract_the_case_id_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("extract_the_case_id_1() called")
+
+    filtered_artifact_0_data_filter_email_artifact = phantom.collect2(container=container, datapath=["filtered-data:filter_email_artifact:condition_1:artifact:*.cef.emailHeaders.Subject","filtered-data:filter_email_artifact:condition_1:artifact:*.id"])
+
+    parameters = []
+
+    # build parameters list for 'extract_the_case_id_1' call
+    for filtered_artifact_0_item_filter_email_artifact in filtered_artifact_0_data_filter_email_artifact:
+        parameters.append({
+            "subject": filtered_artifact_0_item_filter_email_artifact[0],
+        })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.custom_function(custom_function="Phishing_Investigation/Extract_the_case_id", parameters=parameters, name="extract_the_case_id_1", callback=debug_2)
+
+    return
+
+
+def debug_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("debug_2() called")
+
+    extract_the_case_id_1__result = phantom.collect2(container=container, datapath=["extract_the_case_id_1:custom_function_result.data.case_id"])
+
+    extract_the_case_id_1_data_case_id = [item[0] for item in extract_the_case_id_1__result]
+
+    parameters = []
+
+    parameters.append({
+        "input_1": extract_the_case_id_1_data_case_id,
+        "input_2": None,
+        "input_3": None,
+        "input_4": None,
+        "input_5": None,
+        "input_6": None,
+        "input_7": None,
+        "input_8": None,
+        "input_9": None,
+        "input_10": None,
+    })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.custom_function(custom_function="community/debug", parameters=parameters, name="debug_2")
+
+    return
+
 
 def on_finish(container, summary):
     phantom.debug("on_finish() called")
